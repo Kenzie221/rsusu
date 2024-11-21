d0d34d18b1dfdb10a5253c558f89714783ebda97
Kenzie221 <139696688+Kenzie221@users.noreply.github.com>
2024-11-21 15:09:11 +0700

    Update base.js

--- src/http/base.js
+++ src/http/base.js
@@ -1,7 +1,7 @@
 import { QueryClient } from "@tanstack/react-query"
 
 
-export const urlBase = "https://10.6.13.247/iqueue"
+export const urlBase = "http://10.6.13.247/iqueue"
 
 
 export const queryClient = new QueryClient();
